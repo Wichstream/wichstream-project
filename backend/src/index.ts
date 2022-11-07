c389a8908c15312465e58a1a0230919333c9bab3
Edson Junior <ebjr.tux@gmail.com>
2022-11-07 19:34:39 -0300

    Preparando o build do projeto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import http from 'http';
+import http from "http";
 
-import server from './server';
+import server from "./server";
 
 import { createConnection } from "typeorm";
 
-const { PORT } = process.env;
-
-http.createServer({
-}, server)
-  .listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+http.createServer({}, server).listen(5000, '0.0.0.0', () => {
+  console.log(`Server is running...`);
+});
 
 createConnection()
   .then(async () => {
     server.listen(() => {
-      console.log(`CONNECTED TO DB AND SERVER START ON ${PORT}`);
+      console.log(`CONNECTED TO DB AND SERVER START ON 5000`);
     });
   })
   .catch((error) => console.log(error));
